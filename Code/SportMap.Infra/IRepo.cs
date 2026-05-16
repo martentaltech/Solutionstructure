4c630515207e2e3aee8c64e68f4f1de17f54736d
martentaltech <martts@taltech.ee>
2026-05-17 00:36:32 +0300

    Lisasin Query klassi ja uuendasin IRepo liidese

--- Code/SportMap.Infra/IRepo.cs
+++ Code/SportMap.Infra/IRepo.cs
@@ -3,9 +3,22 @@ using SportMap.Data.Common;
 
 namespace SportMap.Infra;
 
+public sealed class Query(Dictionary<string, string> d = null) {
+    public static int[] PageSizes => [7, 15, 25, 50, 100];
+    public int Page => toInt(get(nameof(Page)), 1);
+    public int PageSize => toInt(get(nameof(PageSize)), PageSizes[0]);
+    public string SortBy => get(nameof(SortBy));
+    public string SortDir => get(nameof(SortDir));
+    public string SearchBy => get(nameof(SearchBy));
+    public string SearchStr => get(nameof(SearchStr));
+    private string get(string s) => (d ?? []).TryGetValue(s, out var x) ? x : null;
+    private static int toInt(string s, int def) => int.TryParse(s, out var i) ? i : def;
+}
+
 public interface IRepo<TEntity> where TEntity : BaseEntity {
     Task<TEntity> GetAsync(Guid id);
-    Task<IEnumerable<TEntity>> GetAsync();
+    Task<int> CountAsync(Query q);
+    Task<IEnumerable<TEntity>> GetAsync(Query q);
     Task<TEntity> CreateAsync(TEntity e);
     Task<TEntity> UpdateAsync(TEntity e);
     Task DeleteAsync(Guid id);
@@ -14,5 +27,5 @@ public interface IRepo<TEntity> where TEntity : BaseEntity {
 public interface IMoviesRepo : IRepo<Movie> { }
 public interface ICountriesRepo : IRepo<Country> { }
 public interface ICurrenciesRepo : IRepo<Currency> { }
-public interface IMoniesRepo : IRepo<Money> { }
+public interface IMoneyRepo : IRepo<Money> { }
 public interface ICountryCurrenciesRepo : IRepo<CountryCurrency> { }
