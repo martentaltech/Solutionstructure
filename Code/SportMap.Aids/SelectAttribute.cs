89e70200dc247c51ea272c4ff6e9fbe3bc8dcadf
martentaltech <martts@taltech.ee>
2026-05-20 09:20:00 +0300

    Lisasin SelectAttribute FK dropdown annotatsiooniks

---
+++ Code/SportMap.Aids/SelectAttribute.cs
@@ -0,0 +1,7 @@
+namespace SportMap.Aids;
+
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class SelectAttribute(Type t, string displayProperty = null) : Attribute {
+    public Type EntityType { get; } = t;
+    public string DisplayProperty { get; } = string.IsNullOrWhiteSpace(displayProperty) ? "Name" : displayProperty;
+}
